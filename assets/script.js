8deeccea57e6003878208e66b11eb49505790545
Rachel Iaconianni <iaconiannir@gmail.com>
2020-01-15 19:07:05 -0500

    current branch

--- assets/script.js
+++ assets/script.js
@@ -37,6 +37,25 @@ function handleFormSubmit(event) {
     return false;
   }
 
+  var queryURLOne = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=58044ceb57e67f25a86502f8ce4be039&units=imperial`;
+
+  // make our search with AJAX
+  $.ajax({
+    url: queryURLOne,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    $(".city").html("<h1>" + response.name + "</h1>");
+    $(".description").text(
+      "Weather: " + response.weather[0].description
+    );
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".temp").text("Temperature (F): " + response.main.temp);
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+  });
+  
+
   var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=58044ceb57e67f25a86502f8ce4be039&units=imperial`;
 
   // make our search with AJAX
